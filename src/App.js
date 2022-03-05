f8890fd40af69a311fbdbce4dc725686d4a45a56
Satouri Ridha <satouri.ridha@gmail.com>
2022-03-05 18:02:17 +0100

    JSX Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import logo from './logo.svg';
+import signature from './signature.png';
 import './App.css';
-//import myfirstimage from "../public/imageInPublic.png"
-import logo from './20456790.jpg';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button, Form, NavbarBrand } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap'
 
+import imageInSrc from './imageInSrc.jpg';
+import imageInPublic from './imageInPublic.png';
+import videomp4 from './myVideo.mp4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      <img src="20456790.jpg" alt="myimage" />
-      <img src={logo} alt="Logo" />
-        
-      </header>
+        <div style={{ border:"solid 1px black", maxWidth:'100vw', padding:'20px'}} > 
+        <h1 className="title red">Satouri Ridha</h1>
+        <br ></br>
+        <img  src={imageInSrc} className="imgages" alt="firstimg" />
+        <br ></br>
+        <img src={imageInPublic} className="imgages" alt="scdimage" />
+            {/* Impossible de mettre l image dans le dossier public voir ci dessous
+            Compiled with problems:X
+            ERROR in ./src/App.js 6:0-56
+            Module not found: Error: You attempted to import ../public/imageInPublic.png
+            which falls outside of the project src/ directory. Relative imports outside 
+            of src/ are not supported. */}
+        </div>
+        <video width='320' height='240' controls>
+        <source src={videomp4} type="video/mp4" ></source>
+        </video>
+      
     </div>
   );
 }
